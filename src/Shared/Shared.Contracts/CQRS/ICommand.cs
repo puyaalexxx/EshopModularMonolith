7ab60eec6376f7cb2583d0fc1cf6e2e6a5c57373
Alex <puyaalexxx@gmail.com>
2025-07-21 11:49:11 +0300

    change shared.contract location as it was in the wrong place

---
+++ src/Shared/Shared.Contracts/CQRS/ICommand.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Shared.Contacts.CQRS
+{
+    public interface ICommand : IRequest<Unit>
+    {
+        // This interface is intentionally left empty. It serves as a marker interface for commands in the CQRS pattern.
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+        // This interface is intentionally left empty. It serves as a marker interface for commands in the CQRS pattern.
+    }
+}
---
+++ src/Shared/Shared.Contracts/CQRS/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Contacts.CQRS
+{
+    public interface IQuery<out T> : IRequest<T> where T : notnull
+    {
+        // This interface is intentionally left empty. It serves as a marker interface for queries in the CQRS pattern.
+    }
+}
